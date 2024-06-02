cf0514fd655f3c96e80c6ed27a09cea72c9408aa
jishnu79 <jishnu@toolfe.com>
2024-06-02 13:56:45 +0530

    tailwind config

--- learning/app/page.tsx
+++ learning/app/page.tsx
@@ -1,8 +1,12 @@
+import Hero from "@/Components/Hero";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="overflow-hidden">
-    
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div>
+        <Hero />
+      </div>
     </main>
   );
 }
