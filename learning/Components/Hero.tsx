affbb9b68978d8850f82b7502ac6762540a9e714
jishnu79 <jishnu@toolfe.com>
2024-06-03 08:45:04 +0530

    2nd commit

--- learning/Components/Hero.tsx
+++ learning/Components/Hero.tsx
@@ -1,16 +1,55 @@
 import React from 'react'
 import { Spotlight } from './ui/Sportlight'
+import { TextGenerateEffect } from './ui/TextGeneratee'
+import MagicButton from './ui/MagicButton'
+import { FaLocationArrow } from 'react-icons/fa'
 
 function Hero() {
     return (
         <div className='pb-20 pt-36'>
             <div>
-                <Spotlight className='-top-40 -left-10 md:-left-32 md:-top-20 h-screen'
+                <Spotlight className='-top-40 -left-10 md:-left-32 md:-top-20 
+                h-screen'
                     fill='white' />
                 <Spotlight className='top-10 left-full h-[80vh] w-[50vw]'
                     fill='purple' />
                 <Spotlight className='top-28 left-80 h-[80vh] w-[50vw]'
                     fill='blue' />
+            </div>
+            <div
+                className="h-screen w-full dark:bg-black-100
+                dark:bg-grid-white/[0.03] bg-grid-black-100/[0.2]
+                absolute top-0 left-0 flex items-center justify-center"
+            >
+                <div
+                    className="absolute pointer-events-none inset-0 flex items-center
+                     justify-center dark:bg-black-100
+          [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"
+                />
+            </div>
+            <div className='flex justify-center relative my-20 z-10'>
+                <div className=' max-w-[89vm] md:max-w-2xl lg:max-w-[60vm] flex flex-col 
+                items-center '>
+                    <h2 className='uppercase tracking-widest text-xs text-center 
+                    text-blue-100 max-w-80'>
+                        Dynamic web magic
+                    </h2>
+                    <TextGenerateEffect
+                        className='text-center  text-[40px] md:text-5xl lg:text-6xl '
+                        words='Tranforming Concepts into Seamless experience'
+                    />
+                    <p className='text-center text-white md:tracking-wider mb-4 text-sm md:text-lg
+                    lg:text-2xl'>
+                        Hi , I&apos;m jishnu, a next.js Developer based in kerala
+                    </p>
+                    <a href="#about">
+                        <MagicButton
+                            title='show my work'
+                            icon={<FaLocationArrow />}
+                            position='right'
+                        />
+                    </a>
+                </div>
             </div>
         </div>
     )
